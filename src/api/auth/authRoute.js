585444ad1615303b9e16dfe165026ff0ce58c2a4
sande265 <sandeshsingh265@gmail.com>
2021-07-02 17:25:55 +0545

    added verification of token while changing password

--- src/api/auth/authRoute.js
+++ src/api/auth/authRoute.js
@@ -1,11 +1,12 @@
 const { createUser } = require('../users/userController');
 const { login, resetPassword, changePassword } = require('./authController');
+const { verifyToken } = require('./token_validation');
 const router = require('express').Router();
 
 
 router.post('/signin', login);
 router.post('/signup', createUser)
-router.patch('/change-password', changePassword)
+router.patch('/change-password', verifyToken, changePassword)
 router.post('/reset', resetPassword)
 
 
